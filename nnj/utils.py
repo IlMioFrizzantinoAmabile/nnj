f4e38742939cbc9a758a695f8968e11d4ce90038
FrederikWarburg <frewar1905@gmail.com>
2023-07-06 11:56:43 +0200

    added utils to convert from torch to nnj

---
+++ nnj/utils.py
@@ -0,0 +1,35 @@
+import torch.nn as nn
+
+import nnj
+
+
+def convert_to_nnj(sequential: nn.Sequential) -> nnj.Sequential:
+    model = []
+    for layer in sequential:
+        if layer.__class__.__name__ == "Linear":
+            nnj_layer = getattr(nnj, layer.__class__.__name__)(
+                in_features=layer.in_features,
+                out_features=layer.out_features,
+            )
+            nnj_layer.weight.data = layer.weight.data
+            nnj_layer.bias.data = layer.bias.data
+        elif layer.__class__.__name__ == "Conv2d":
+            nnj_layer = getattr(nnj, layer.__class__.__name__)(
+                in_channels=layer.in_channels,
+                out_channels=layer.out_channels,
+                kernel_size=layer.kernel_size,
+            )
+            nnj_layer.weight.data = layer.weight.data
+            nnj_layer.bias.data = layer.bias.data
+        elif "Pool" in layer.__class__.__name__:
+            nnj_layer = getattr(nnj, layer.__class__.__name__)(
+                kernel_size=layer.kernel_size,
+            )
+        else:
+            nnj_layer = getattr(nnj, layer.__class__.__name__)()
+
+        model.append(nnj_layer)
+
+    model = nnj.Sequential(*model, add_hooks=True)
+
+    return model
