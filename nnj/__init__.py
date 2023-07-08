a837c8b7d9ff6792fc7964884e390a94eab58fc1
Frederik Warburg frwa@dtu.dk <frewar1905@gmail.com>
2023-07-08 15:45:54 +0000

    add sigmoid layer

--- nnj/__init__.py
+++ nnj/__init__.py
@@ -13,6 +13,7 @@ from nnj.sequential import Sequential  # isort:skip
 ### non-parametric layers ###
 from nnj.tanh import Tanh  # isort:skip
 from nnj.relu import ReLU  # isort:skip
+from nnj.sigmoid import Sigmoid  # isort:skip
 
 #############################
 ### utils                 ###
