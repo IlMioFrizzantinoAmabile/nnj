improved tests jacobians and some bug fix

nnj/linear.py
@@ -99,7 +99,7 @@ class Linear(nn.Linear, AbstractJacobian):
         from_diag: bool = False,
         to_diag: bool = False,
         diag_backprop: bool = False,
-    ) -> Union[Tensor, Tuple]:
+    ) -> Tensor:
         """
         jacobian matrix jacobian.T product
         """

nnj/relu.py
@@ -19,7 +19,7 @@ class ReLU(AbstractDiagonalJacobian, nn.ReLU):
         if wrt == "input":
             if val is None:
                 val = self.forward(x)
-            diag_jacobian = (val > 0.0).type(val.dtype)
+            diag_jacobian = (val > 0.0).type(val.dtype).reshape(val.shape[0], -1)
             if diag:
                 return diag_jacobian
             else:

tests/jacobians.py
@@ -11,30 +11,51 @@ import torch
 
 import nnj
 
-# define some input data
-xs = [
-    torch.randn(7, 3),
-    torch.ones(7, 3),
-    torch.randn(7, 3) + torch.ones(7, 3),
-    10 * torch.rand(7, 3),
-]
+# define input sizes
+batch_size = 7
+shape_1D = (3,)
+shape_2D = (3, 4)
+shape_3D = (2, 3, 4)
 
-# get all the layers
-to_test_easy = [
-    nnj.Linear(3, 5, bias=False),
-    nnj.Linear(3, 5),
+# define input data
+xs_nD = []
+for shape in [shape_1D, shape_2D, shape_3D]:
+    xs_nD.append(
+        [
+            torch.randn(batch_size, *shape),
+            torch.ones(batch_size, *shape),
+            torch.randn(batch_size, *shape) + torch.ones(batch_size, *shape),
+            10 * torch.rand(batch_size, *shape),
+        ]
+    )
+xs_1D, xs_2D, xs_3D = xs_nD
+
+# define the layers to test
+layers_on_allx = [
     nnj.Tanh(),
     nnj.ReLU(),
     nnj.Sigmoid(),
     nnj.Sinusoidal(),
     nnj.TruncExp(),
     nnj.Softplus(),
+    nnj.Flatten(),
 ]
-to_test_advanced = [
-    nnj.Sequential(nnj.Linear(3, 5), nnj.Tanh(), nnj.Linear(5, 13), add_hooks=True),
-    nnj.Sequential(nnj.Linear(3, 5), nnj.ReLU(), nnj.Linear(5, 13), add_hooks=True),
+layers_on_x1D = [
+    nnj.Linear(*shape_1D, 5, bias=False),
+    nnj.Linear(*shape_1D, 5),
+    nnj.Sequential(nnj.Linear(*shape_1D, 5), nnj.Tanh(), nnj.Linear(5, 13), add_hooks=True),
+    nnj.Sequential(nnj.Linear(*shape_1D, 5), nnj.ReLU(), nnj.Linear(5, 13), add_hooks=True),
     nnj.Sequential(
-        nnj.Linear(3, 5),
+        nnj.Linear(*shape_1D, 6),
+        nnj.Tanh(),
+        nnj.Reshape(1, 2, 3),
+        nnj.Upsample(scale_factor=2),
+        nnj.Flatten(),
+        nnj.Tanh(),
+        add_hooks=True,
+    ),
+    nnj.Sequential(
+        nnj.Linear(*shape_1D, 5),
         nnj.Tanh(),
         nnj.Linear(5, 2),
         nnj.Tanh(),
@@ -48,7 +69,7 @@ to_test_advanced = [
         add_hooks=True,
     ),
     nnj.Sequential(
-        nnj.Linear(3, 5),
+        nnj.Linear(*shape_1D, 5),
         nnj.Tanh(),
         nnj.Sequential(
             nnj.Linear(5, 5),
@@ -61,60 +82,105 @@ to_test_advanced = [
         nnj.Linear(2, 13),
         add_hooks=True,
     ),
-    nnj.Sigmoid(),
-    nnj.Softplus(),
-    nnj.TruncExp(),
 ]
+layers_on_x2D = [
+    nnj.Reshape(6, 2),
+]
+layers_on_x3D = [
+    nnj.Upsample(scale_factor=2),
+    nnj.Upsample(scale_factor=3),
+    nnj.Reshape(6, 2, 2),
+    nnj.Sequential(
+        nnj.Flatten(),
+        nnj.Linear(24, 6),
+        nnj.Tanh(),
+        nnj.Reshape(1, 2, 3),
+        add_hooks=True,
+    ),
+]
+
+to_test_advanced = []
+to_test_easy = []
 
 
 def test_jacobian_wrt_input():
-    for layer in to_test_easy + to_test_advanced:
-        for x in xs:
-            val = layer(x)
-            assert len(x.shape) == 2 and len(val.shape) == 2
-            batch_size, out_size = val.shape
-            _, in_size = x.shape
+    # define test on specific layer and input
+    def test_jacobian_wrt_input_on(layer, x):
+        val = layer(x)
+        batch_size = x.shape[0]
+        in_size = x[0].numel()
+        out_size = val[0].numel()
+        out_shape = val.shape[1:]
 
-            jacobian_nnj = layer.jacobian(x, None, wrt="input")
+        jacobian_nnj = layer.jacobian(x, None, wrt="input")
 
-            jacobian_nn = torch.zeros(batch_size, out_size, in_size)
-            for o in range(out_size):
-                basis_vector = torch.zeros_like(val)
-                for b in range(batch_size):
-                    basis_vector[b, o] = 1.0
-                row_o = torch.autograd.functional.vjp(layer, x, v=basis_vector)[1]
-                jacobian_nn[:, o, :] = row_o
+        jacobian_nn = torch.zeros(batch_size, out_size, in_size)
+        for o in range(out_size):
+            basis_vector = torch.zeros_like(val).reshape(batch_size, out_size)
+            for b in range(batch_size):
+                basis_vector[b, o] = 1.0
+            basis_vector = basis_vector.reshape(batch_size, *out_shape)
+            row_o = torch.autograd.functional.vjp(layer, x, v=basis_vector)[1]
+            row_o = row_o.reshape(batch_size, in_size)
+            jacobian_nn[:, o, :] = row_o
 
-            assert jacobian_nnj.shape == jacobian_nn.shape
-            assert torch.isclose(jacobian_nnj, jacobian_nn, atol=1e-4).all()
+        assert jacobian_nnj.shape == jacobian_nn.shape
+        assert torch.isclose(jacobian_nnj, jacobian_nn, atol=1e-4).all()
+
+    # run tests on all combinations
+    for layer in layers_on_allx:
+        for x in xs_1D + xs_2D + xs_3D:
+            test_jacobian_wrt_input_on(layer, x)
+    for layer in layers_on_x1D:
+        for x in xs_1D:
+            test_jacobian_wrt_input_on(layer, x)
+    for layer in layers_on_x2D:
+        for x in xs_2D:
+            test_jacobian_wrt_input_on(layer, x)
+    for layer in layers_on_x3D:
+        for x in xs_3D:
+            test_jacobian_wrt_input_on(layer, x)
 
 
 def test_jacobian_wrt_weight():
-    for layer in to_test_easy + to_test_advanced:
-        for x in xs:
-            val = layer(x)
-            assert len(val.shape) == 2
-            batch_size, out_size = val.shape
-            _, in_size = x.shape
-            param_size = layer._n_params
+    # define test on specific layer and input
+    def test_jacobian_wrt_weight_on(layer, x):
+        val = layer(x)
+        batch_size = x.shape[0]
+        out_size = val[0].numel()
+        param_size = layer._n_params
 
-            jacobian_nnj = layer.jacobian(x, None, wrt="weight")
-            if layer._n_params == 0:
-                assert jacobian_nnj is None
-                continue
+        jacobian_nnj = layer.jacobian(x, None, wrt="weight")
+        if layer._n_params == 0:
+            assert jacobian_nnj is None
+            return
 
-            jacobian_nn = torch.zeros(batch_size, out_size, param_size)
-            for b in range(batch_size):
-                val = layer(x[b : b + 1, :])
-                for o in range(out_size):
-                    loss = val[0, o]
-                    loss.backward(retain_graph=True)
-                    row_o = []
-                    for p in layer.parameters():
-                        row_o.append(torch.clone(p.grad).view(-1))
-                        p.grad.zero_()
-                    row_o = torch.cat(row_o)
-                    jacobian_nn[b, o, :] = row_o
+        jacobian_nn = torch.zeros(batch_size, out_size, param_size)
+        for b in range(batch_size):
+            val = layer(x[b : b + 1]).reshape(out_size)
+            for o in range(out_size):
+                loss = val[o]
+                loss.backward(retain_graph=True)
+                row_o = []
+                for p in layer.parameters():
+                    row_o.append(torch.clone(p.grad).view(-1))
+                    p.grad.zero_()
+                row_o = torch.cat(row_o)
+                jacobian_nn[b, o, :] = row_o
 
-            assert jacobian_nnj.shape == jacobian_nn.shape
-            assert torch.isclose(jacobian_nnj, jacobian_nn, atol=1e-4).all()
+        assert jacobian_nnj.shape == jacobian_nn.shape
+        assert torch.isclose(jacobian_nnj, jacobian_nn, atol=1e-4).all()
+
+    # run tests on all combinations
+    for layer in layers_on_allx:
+        for x in xs_1D + xs_2D + xs_3D:
+            test_jacobian_wrt_weight_on(layer, x)
+    for layer in layers_on_x1D:
+        for x in xs_1D:
+            test_jacobian_wrt_weight_on(layer, x)
+    for layer in layers_on_x2D:
+        for x in xs_2D:
+            test_jacobian_wrt_weight_on(layer, x)
+    for layer in layers_on_x3D:
+        for x in xs_3D:
+            test_jacobian_wrt_weight_on(layer, x)